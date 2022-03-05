1747b2bd09c70f2348a511aadb5ff933ef238ad5
broshan989 <bhogeroshan98@gmail.com>
2022-03-05 13:12:39 +0530

    Creating the spring boot project and it's structure

---
+++ blogging-app-springboot/src/main/java/com/blog/app/BloggingAppSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.blog.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingAppSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingAppSpringbootApplication.class, args);
+	}
+
+}
---
+++ blogging-app-springboot/src/test/java/com/blog/app/BloggingAppSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingAppSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
