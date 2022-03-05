4c98fe382e8a3370e2d44e8fa623c41dc164eabe
broshan989 <bhogeroshan98@gmail.com>
2022-03-05 22:15:06 +0530

    creating post entity and repository

---
+++ blogging-app-springboot/src/main/java/com/blog/app/entity/Post.java
@@ -0,0 +1,28 @@
+package com.blog.app.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String title;
+
+    @Column(length = 100, nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String content;
+}
